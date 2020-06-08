76dcbf390766c4772daf3950e21904f72f642109
dawsonhammdev <hammsdesigns@gmail.com>
2020-06-08 18:17:54 -0500

    I have met MVP all tests are passing

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -7,6 +7,9 @@ class City:
     self.lat = lat
     self.lon = lon
 
+  def __str__(self):
+    return f"{self.name}, {self.lat}, {self.lon}"
+
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -25,13 +28,12 @@ cities = []
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   with open('cities.csv', newline = '') as csvfile:
+    next(csvfile)
     # For each city record, create a new City instance and add it to the 
   # `cities` list
     for row in csv.reader(csvfile):
-      cities.append(row[0])
-      print(cities)
+      cities.append(City(row[0], float(row[3]), float(row[4])))
 
-  
     
     return cities
 
@@ -39,8 +41,7 @@ cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
-  def __str__(self):
-    return '%.2d:%.2d:%.2d' % (self.name, self.lat, self.lon)
+  
     print(c)
 
 # STRETCH GOAL!
